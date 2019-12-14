03a9bc0f3972ac8f30f399f8be7aa59db3d6e317
Dickson <jdickson@agoda.local>
2019-12-14 11:47:15 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dotnet-build-metrics
+# Dotnet Build Metrics
+
+MS Build task library compatible with MSBuild and dotnet core CLI.
+
+Designed to collect compile time metrics from developers local workstation and log for later analysis of Time to Dev Feedback.
+
+Currently it only outputs to the command prompt.
+
+Example in Visual Studio using MSBuild.
+
+![](doc/img/VSBuildOutput.png)
+
+Exmaple in Dotnet core CLI.
+
+![](doc/img/DotnetCLIBuildTimeOutput.png)
+
---
+++ src/Agoda.Builds.Metrics/MeasureBuildTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Agoda.Builds.Metrics
+{
+    public class MeasureBuildTime : Task
+    {
+        public string StartDateTime { get; set; }
+        public string EndDateTime { get; set; }
+        [Output]
+        public string DebugOutput { get; set; }
+        public override bool Execute()
+        {
+            DebugOutput = (DateTime.Parse(EndDateTime) - DateTime.Parse(StartDateTime)).TotalMilliseconds.ToString();
+            
+            return true;
+        }
+    }
+}
