c91d709eb2f601a8ca3573e733445fdf61bbeed3
ratchapol-an <ratchapol.an@gmail.com>
2020-02-12 15:44:57 +0700

    post data to api (#1)

--- src/Agoda.Builds.Metrics/MeasureBuildTime.cs
+++ src/Agoda.Builds.Metrics/MeasureBuildTime.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Linq;
+using LibGit2Sharp;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Agoda.Builds.Metrics
 {
@@ -15,7 +20,46 @@ namespace Agoda.Builds.Metrics
         public override bool Execute()
         {
             DebugOutput = (DateTime.Parse(EndDateTime) - DateTime.Parse(StartDateTime)).TotalMilliseconds.ToString();
-            
+            try
+            {
+                var username = Environment.UserName;
+                var cpuCount = Environment.ProcessorCount;
+                var hostname = Environment.MachineName;
+                var platform = Environment.OSVersion.Platform;
+                var os = Environment.OSVersion.VersionString;
+                var gitBranch = string.Empty;
+                using (var repo = new Repository(Environment.CurrentDirectory))
+                {
+                    gitBranch = repo.Branches.Where(b => !b.IsRemote && b.IsCurrentRepositoryHead).FirstOrDefault().FriendlyName;
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.Timeout = TimeSpan.FromMilliseconds(100);
+                    httpClient.BaseAddress = new Uri("http://build-mertics");
+                    var data = new
+                    {
+                        username = Environment.UserName,
+                        cpuCount = Environment.ProcessorCount,
+                        hostname = Environment.MachineName,
+                        platform = Environment.OSVersion.Platform,
+                        os = Environment.OSVersion.VersionString,
+                        timeTaken = DebugOutput,
+                        gitBranch
+                    };
+                    var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+                    var response = httpClient.PostAsync("/api/buildmetric/track", content).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Log.LogError(response.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex.Message);
+            }
+
             return true;
         }
     }
