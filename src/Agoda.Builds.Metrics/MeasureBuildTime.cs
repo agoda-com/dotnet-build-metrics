f6eb2651fe6f4521eee1b8d9e618e506389d7971
Virasak Dungsrikaew <virasak@gmail.com>
2023-07-10 16:29:36 +0700

    fix wrong endpoint for sending build time (#37)
    Co-authored-by: Virasak Dungrikaew <virasak.dungsrikaew@agoda.com>

--- src/Agoda.Builds.Metrics/MeasureBuildTime.cs
+++ src/Agoda.Builds.Metrics/MeasureBuildTime.cs
@@ -50,7 +50,7 @@ namespace Agoda.Builds.Metrics
                     gitContext: gitContext
                 );
 
-                DevFeedbackPublisher.Publish(ApiEndPoint, data, DevLocalDataType.NUnit);
+                DevFeedbackPublisher.Publish(ApiEndPoint, data, DevLocalDataType.Build);
             }
             catch (GitContextException ex)
             {
