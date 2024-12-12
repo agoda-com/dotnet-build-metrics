ac4cc3337d2f51542e8fbbc1dc1255e5f9786a70
Joel Dickson <9032274+joeldickson@users.noreply.github.com>
2024-12-12 18:21:35 +0700

    Add tags for is QA (#45)
    * Add tags for is QA
    
    * make changes
    
    * refactor
    
    ---------
    
    Co-authored-by: jdickson <joel.dickson@agoda.com>

--- src/Agoda.DevFeedback.AspNetStartup/TimedStartupPublisher.cs
+++ src/Agoda.DevFeedback.AspNetStartup/TimedStartupPublisher.cs
@@ -1,5 +1,8 @@
 ﻿using Agoda.DevFeedback.Common;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace Agoda.DevFeedback.AspNetStartup
@@ -8,14 +11,28 @@ namespace Agoda.DevFeedback.AspNetStartup
     {
         public static void Publish(string type, TimeSpan timeTaken)
         {
+            const string TagPrefix = "DEVFEEDBACK_TAG_";
+
             var gitContext = GitContextReader.GetGitContext();
+
+            // Use LINQ and dictionary comprehension for cleaner tag collection
+            var tags = Environment.GetEnvironmentVariables()
+                .Cast<DictionaryEntry>()
+                .Select(entry => (Key: entry.Key.ToString()!, Value: entry.Value?.ToString()))
+                .Where(entry => entry.Key.StartsWith(TagPrefix, StringComparison.OrdinalIgnoreCase)
+                                && !string.IsNullOrEmpty(entry.Value))
+                .ToDictionary(
+                    entry => entry.Key[TagPrefix.Length..].ToLowerInvariant(),
+                    entry => entry.Value!
+                );
 
             var result = new DevFeedbackData(
                 metricsVersion: Assembly.GetExecutingAssembly().GetName().Version?.ToString(),
                 type: type,
                 projectName: Assembly.GetEntryAssembly()?.GetName().Name,
                 timeTaken: timeTaken.TotalMilliseconds.ToString(),
-                gitContext: gitContext
+                gitContext: gitContext,
+                tags: tags
             );
 
             DevFeedbackPublisher.Publish(apiEndpoint: null, result, DevLocalDataType.Build);
