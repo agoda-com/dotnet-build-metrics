79463090175875fd9c022b0ea0bafcde0afe993e
jdickson <joel.dickson@agoda.com>
2024-12-12 17:29:56 +0700

    make changes

--- src/Agoda.DevFeedback.AspNetStartup/TimedStartupPublisher.cs
+++ src/Agoda.DevFeedback.AspNetStartup/TimedStartupPublisher.cs
@@ -1,5 +1,6 @@
 ﻿using Agoda.DevFeedback.Common;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -10,10 +11,26 @@ namespace Agoda.DevFeedback.AspNetStartup
         public static void Publish(string type, TimeSpan timeTaken)
         {
             var gitContext = GitContextReader.GetGitContext();
-            
-            var tags = new Dictionary<string, string>();
 
-            if(Environment.GetEnvironmentVariable("DISABLE_QA") != null) tags.Add("isDisableQa", Environment.GetEnvironmentVariable("DISABLE_QA"));
+            var tags = new Dictionary<string, string>();
+            const string TAG_PREFIX = "DEVFEEDBACK_TAG_";
+
+            // Get all environment variables and filter for ones starting with DEVFEEDBACK_TAG_
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            {
+                string key = de.Key.ToString();
+                if (key.StartsWith(TAG_PREFIX, StringComparison.OrdinalIgnoreCase))
+                {
+                    // Remove the prefix to get the clean tag name
+                    string tagName = key.Substring(TAG_PREFIX.Length).ToLowerInvariant();
+                    string tagValue = de.Value?.ToString() ?? string.Empty;
+
+                    if (!string.IsNullOrEmpty(tagValue))
+                    {
+                        tags.Add(tagName, tagValue);
+                    }
+                }
+            }
 
             var result = new DevFeedbackData(
                 metricsVersion: Assembly.GetExecutingAssembly().GetName().Version?.ToString(),
